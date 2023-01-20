fba0856b64a0c467f42224b258ce7f5136146b8e
micheal idam <princemicare@gmail.com>
2023-01-20 04:37:36 +0100

    Changed the toggle style for the light and dark modes

--- assets/js/theme-change.js
+++ assets/js/theme-change.js
@@ -4,18 +4,18 @@ const currentTheme = localStorage.getItem('theme');
 if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
   
-    if (currentTheme === 'dark') {
+    if (currentTheme === 'light') {
         toggleSwitch.checked = true;
     }
 }
 
 function switchTheme(e) {
     if (e.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        localStorage.setItem('theme', 'dark');
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
     }
-    else {        document.documentElement.setAttribute('data-theme', 'light');
-          localStorage.setItem('theme', 'light');
+    else {        document.documentElement.setAttribute('data-theme', 'dark');
+          localStorage.setItem('theme', 'dark');
     }    
 }
 
